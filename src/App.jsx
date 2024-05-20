d56b28db1675444851eaeb9c0cb1eb717c2f66ff
kimyong <right4570@naver.com>
2024-05-20 15:50:46 +0900

    kimyong 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+
 export default function App() {
+  const [num, setNum] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handlePlus = () => {
+    setResult((prev) => Number(prev) + Number(num));
+    setNum("");
+  };
+  const handlemiuns = () => {
+    setResult((prev) => Number(prev) - Number(num));
+    setNum("");
+  };
+
+  const reset = () => {
+    setNum("");
+    setResult("");
+  };
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          value={num}
+          onChange={(e) => {
+            setNum(e.target.value);
+          }}
+        />{" "}
+        만큼을 <button onClick={handlePlus}>더할게요</button>{" "}
+        <button onClick={handlemiuns}>뺄게요</button>
+        <button onClick={reset}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result ? result : 0}</p>
       </div>
     </div>
   );
